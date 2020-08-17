855da60b0956e70c14c33ed9f7572907de4ead09
ANNA NEVEROVA <neverova.anya@gmail.com>
2020-08-17 00:22:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+  string_3 = 'I'
+
+string_11 = 'Hello'
+string_12 = 'World'
+
+print ('{} wonderful {}' .format(string_11, string_12))
+
+
+string_13 = 'I like Python'
+
+print(string_13.upper())
+print(string_13.lower())
+print(string_13.title())
+print(string_13.capitalize())
+
+
+
+
+print('Hello! Convert miles to km.')
+miles = input('How many miles you\'d like to convert: ')
+km = float(miles) * 1.6
+print(f' Well. This distance in kilometers = {km}')
+
+
+conditions
+if weather == 'sunny':
+ print('Have a nice day')
+ elif weather == 'rainy':
+   print('Take an umbrella')
+   elif weather == 'cold':
+     print('Put on a jacket')
+     else:
+       print('Stay at home')
+
+
+
+x = input('Input a number')
+if x < 10:
+  print('Good value')
+elif x < 25:
+  print('Not bad')
+else: 
+  print('Wrong value')
+
+
+if x >= 20:
+  print('Hello')
+  
